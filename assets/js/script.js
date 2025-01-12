472103a3fdb496916285fd3faf6caffee929b7dc
Kleijn95 <kleijn95@gmail.com>
2025-01-12 17:07:47 +0100

    small change

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.getElementById("navbar");
   const navSection = document.getElementById("nav");
   const button = document.querySelector("#navbar button");
-  const offset = 150; // Modificato l'offset a 150px
+  const offset = 150;
 
   window.addEventListener("scroll", function () {
     const navBottom = navSection.getBoundingClientRect().bottom;
 
     if (navBottom <= offset) {
-      navbar.style.backgroundColor = "#ffffff"; // Cambia colore della navbar
-      navbar.classList.remove("hidden"); // Mostra navbar
-      button.style.backgroundColor = "#28a745"; // Cambia colore del bottone
-      button.style.color = "#ffffff"; // Cambia colore del testo del bottone
+      navbar.style.backgroundColor = "#ffffff";
+      navbar.classList.remove("hidden");
+      button.style.backgroundColor = "#28a745";
+      button.style.color = "#ffffff";
     } else {
-      navbar.style.backgroundColor = "#ffc017"; // Ripristina colore della navbar
-      navbar.classList.add("hidden"); // Nascondi navbar
-      button.style.backgroundColor = ""; // Ripristina colore del bottone
-      button.style.color = ""; // Ripristina colore del testo del bottone
+      navbar.style.backgroundColor = "#ffc017";
+      navbar.classList.add("hidden");
+      button.style.backgroundColor = "";
+      button.style.color = "";
     }
   });
 });
